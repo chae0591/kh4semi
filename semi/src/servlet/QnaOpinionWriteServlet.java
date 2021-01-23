73154a7f6e5afc807cb8aeb5fd55bf1404a5b1c8
BRAIN <BRAIN@DESKTOP-33B6BVF>
2021-01-23 23:10:19 +0900

    qna_board 댓글 서블릿 업로드

--- semi/src/servlet/QnaOpinionWriteServlet.java
+++ semi/src/servlet/QnaOpinionWriteServlet.java
@@ -1,5 +1,25 @@
 package servlet;
 
-public class QnaOpinionWriteServlet {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/qna_board/opinion_write.do")
+public class QnaOpinionWriteServlet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
 
 }
